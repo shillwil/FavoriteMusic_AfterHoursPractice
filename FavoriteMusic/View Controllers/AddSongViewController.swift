c88ca08f40e3cabd4113af1da812268616f5f2aa
shillwil <alex.shillingford@icloud.com>
2019-06-06 20:02:41 -0600

    Added AddSongViewController and linked it up

---
+++ FavoriteMusic/View Controllers/AddSongViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddSongViewController.swift
+//  FavoriteMusic
+//
+//  Created by Alex Shillingford on 6/6/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class AddSongViewController: UIViewController {
+
+    @IBOutlet weak var songNameTextField: UITextField!
+    @IBOutlet weak var songGenreTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addSongTapped(_ sender: Any) {
+    }
+    
+
+}
